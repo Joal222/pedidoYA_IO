52b22f1a72be317eef30f47b264d70c432d5bb90
Jonathan Guamuch <joelmorales0598@gmail.com>
2024-08-29 15:54:46 -0600

    Base JWT - eliminación de entidades

--- src/main/java/FormatoBase/proyectoJWT/model/repository/ProveedoresRepository.java
+++
@@ -1,9 +0,0 @@
-package FormatoBase.proyectoJWT.model.repository;
-
-import FormatoBase.proyectoJWT.model.entity.Proveedores;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface ProveedoresRepository extends JpaRepository<Proveedores, Integer>{
-
-}
